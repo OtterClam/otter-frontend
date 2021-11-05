0b41e2a292149389da8d56bd8d869f4b426ec818
Otter <king@otterclam.finance>
2021-11-05 11:34:04 +0800

    fix: add liquidity url

--- src/helpers/lp-url.ts
+++ src/helpers/lp-url.ts
@@ -5,7 +5,7 @@ export const lpURL = (bond: string, networkID: number): string => {
   const addresses = getAddresses(networkID);
 
   if (bond === BONDS.mai_clam) {
-    return `https://quickswap.exchange/#/pool/${addresses.MAI_ADDRESS}/${addresses.CLAM_ADDRESS}`;
+    return `https://quickswap.exchange/#/add/${addresses.MAI_ADDRESS}/${addresses.CLAM_ADDRESS}`;
   }
 
   throw Error(`LP url doesn't support: ${bond}`);
