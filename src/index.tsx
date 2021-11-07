e5b91cd6ab5d808db955fa6a672431a950867428
Ryoichi Kaede <ryoichi.kaede@otterclam.finance>
2021-11-07 18:44:20 +0800

    refactor: move ThemeProvider inside Root

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,11 @@ import Root from './Routes';
 import store from './store/store';
 import { Provider } from 'react-redux';
 import { Web3ContextProvider } from './hooks';
-import { ThemeProvider } from '@material-ui/core/styles';
-import { light as lightTheme } from './themes';
 
 ReactDOM.render(
   <Web3ContextProvider>
     <Provider store={store}>
-      <ThemeProvider theme={lightTheme}>
-        <Root />
-      </ThemeProvider>
+      <Root />
     </Provider>
   </Web3ContextProvider>,
   document.getElementById('root'),
