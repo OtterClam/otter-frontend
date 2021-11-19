bc0ffd47cdcd8f4be96fd09937e0392001998363
Otter <king@otterclam.finance>
2021-11-20 00:11:56 +0800

    chore: hide new bonds by default

--- src/constants/bonds.ts
+++ src/constants/bonds.ts
@@ -1,8 +1,10 @@
 import { getAddresses } from '.';
 
-export type BondKey = 'mai' | 'mai_clam' | 'mai-v1' | 'mai_clam_v2-v1' | 'mai_clam-v1';
+// export type BondKey = 'mai' | 'mai_clam' | 'mai-v1' | 'mai_clam_v2-v1' | 'mai_clam-v1';
+export type BondKey = 'mai-v1' | 'mai_clam_v2-v1' | 'mai_clam-v1';
 
-export const BondKeys: BondKey[] = ['mai', 'mai_clam', 'mai-v1', 'mai_clam-v1', 'mai_clam_v2-v1'];
+// export const BondKeys: BondKey[] = ['mai', 'mai_clam', 'mai-v1', 'mai_clam-v1', 'mai_clam_v2-v1'];
+export const BondKeys: BondKey[] = ['mai-v1', 'mai_clam-v1', 'mai_clam_v2-v1'];
 
 export interface Bond {
   key: BondKey;
@@ -21,24 +23,24 @@ type BondMap = {
 export function listBonds(chainId: number): BondMap {
   const { BONDS, RESERVES, MAI_ADDRESS, CLAM_ADDRESS } = getAddresses(chainId);
   return {
-    mai: {
-      key: 'mai',
-      name: 'MAI',
-      address: BONDS.MAI,
-      reserve: RESERVES.MAI,
-      type: 'token',
-      lpUrl: '',
-      deprecated: false,
-    },
-    mai_clam: {
-      key: 'mai_clam',
-      name: 'CLAM2-MAI LP',
-      address: BONDS.MAI_CLAM,
-      reserve: RESERVES.MAI_CLAM,
-      type: 'lp',
-      lpUrl: `https://quickswap.exchange/#/add/${MAI_ADDRESS}/${CLAM_ADDRESS}`,
-      deprecated: false,
-    },
+    // mai: {
+    //   key: 'mai',
+    //   name: 'MAI',
+    //   address: BONDS.MAI,
+    //   reserve: RESERVES.MAI,
+    //   type: 'token',
+    //   lpUrl: '',
+    //   deprecated: false,
+    // },
+    // mai_clam: {
+    //   key: 'mai_clam',
+    //   name: 'CLAM2-MAI LP',
+    //   address: BONDS.MAI_CLAM,
+    //   reserve: RESERVES.MAI_CLAM,
+    //   type: 'lp',
+    //   lpUrl: `https://quickswap.exchange/#/add/${MAI_ADDRESS}/${CLAM_ADDRESS}`,
+    //   deprecated: false,
+    // },
     'mai-v1': {
       key: 'mai-v1',
       name: 'MAI(v1)',
--- src/helpers/price-units.tsx
+++ src/helpers/price-units.tsx
@@ -3,7 +3,7 @@ import { BondKey } from 'src/constants';
 import { ReactComponent as MAI } from '../assets/tokens/MAI.svg';
 
 export const priceUnits = (bond: BondKey) => {
-  if (bond === 'mai') return <SvgIcon component={MAI} viewBox="0 0 32 32" style={{ height: '15px', width: '15px' }} />;
+  // if (bond === 'mai') return <SvgIcon component={MAI} viewBox="0 0 32 32" style={{ height: '15px', width: '15px' }} />;
 
   return '$';
 };
