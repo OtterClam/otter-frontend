6ec1352fdb9f3dc489a5ebf8d37428a28f7d9fd6
Po-Ying Chen <pchen@appaegis.com>
2021-11-25 22:27:54 +0800

    feat: update bond icon style

--- src/helpers/get-pair-image.tsx
+++ src/helpers/get-pair-image.tsx
@@ -2,13 +2,23 @@ import { SvgIcon } from '@material-ui/core';
 import { ReactComponent as CLAM } from '../assets/tokens/CLAM.svg';
 import { ReactComponent as MAI } from '../assets/tokens/MAI.svg';
 
+const iconStyle = {
+  height: '32px',
+  width: '32px',
+};
+
+const secondIconStyle = {
+  ...iconStyle,
+  transform: 'translateX(-10px)',
+  zIndex: -1,
+};
+
 export function getPairImage(name: string) {
   if (name.indexOf('mai') >= 0)
     return (
       <>
-        <SvgIcon component={CLAM} viewBox="0 0 32 32" style={{ height: '32px', width: '32px' }} />
-        <div style={{ width: '10px' }} />
-        <SvgIcon component={MAI} viewBox="0 0 32 32" style={{ height: '32px', width: '32px' }} />
+        <SvgIcon component={CLAM} viewBox="0 0 32 32" style={iconStyle} />
+        <SvgIcon component={MAI} viewBox="0 0 32 32" style={secondIconStyle} />
       </>
     );
 
