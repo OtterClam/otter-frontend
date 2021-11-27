cc3a3fb0dc500130b4a7368e30d25b4e9a43055f
Po-Ying Chen <poying.me@gmail.com>
2021-11-27 17:02:40 +0800

    feat: fix firefox displaying issue

--- src/helpers/get-pair-image.tsx
+++ src/helpers/get-pair-image.tsx
@@ -5,22 +5,24 @@ import { ReactComponent as MAI } from '../assets/tokens/MAI.svg';
 const iconStyle = {
   height: '32px',
   width: '32px',
+  zIndex: 1,
 };
 
 const secondIconStyle = {
   ...iconStyle,
   transform: 'translateX(-10px)',
-  zIndex: -1,
+  zIndex: 0,
 };
 
 export function getPairImage(name: string) {
-  if (name.indexOf('mai') >= 0)
+  if (name.indexOf('mai') >= 0) {
     return (
       <>
         <SvgIcon component={CLAM} viewBox="0 0 32 32" style={iconStyle} />
         <SvgIcon component={MAI} viewBox="0 0 32 32" style={secondIconStyle} />
       </>
     );
+  }
 
   throw Error(`Pair image doesn't support: ${name}`);
 }
