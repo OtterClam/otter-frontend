5762bd235f9423d35bad18c575a65f5045a1ee2a
Otter <king@otterclam.finance>
2021-12-07 15:42:02 +0800

    feat: remove on-chain data loading from landing

--- src/Routes/Landing.tsx
+++ src/Routes/Landing.tsx
@@ -1,26 +1,7 @@
-import { useEffect, useCallback } from 'react';
-import { useDispatch } from 'react-redux';
-import { useWeb3Context } from '../hooks';
-import { loadAppDetails } from '../store/slices/app-slice';
 import Landing from '../views/Landing';
 import './style.scss';
 
 function App() {
-  const dispatch = useDispatch();
-
-  const { readOnlyProvider, chainID, connected } = useWeb3Context();
-
-  const loadApp = useCallback(
-    loadProvider => {
-      dispatch(loadAppDetails({ networkID: chainID, provider: loadProvider }));
-    },
-    [connected],
-  );
-
-  useEffect(() => {
-    loadApp(readOnlyProvider);
-  }, []);
-
   return <Landing />;
 }
 
--- src/views/Landing/components/Stat/index.tsx
+++ src/views/Landing/components/Stat/index.tsx
@@ -7,6 +7,7 @@ import './stat.scss';
 function Stat() {
   const [stakingAPY, setStakingAPY] = useState<number | null>(null);
   const [treasuryBalance, setTreasuryBalance] = useState<number | null>(null);
+  const [tvl, setTvl] = useState<number | null>(null);
 
   useEffect(() => {
     apollo(`
@@ -14,23 +15,37 @@ query {
   protocolMetrics(first: 1, orderBy: timestamp, orderDirection: desc) {
     treasuryMarketValue
     currentAPY
+    totalValueLocked
   }
 }`).then(r => {
       const latestMetrics = (r as any).data.protocolMetrics[0];
       setTreasuryBalance(latestMetrics.treasuryMarketValue);
       setStakingAPY(latestMetrics.currentAPY);
+      setTvl(latestMetrics.totalValueLocked);
     });
   });
 
   return (
     <div className="landing-footer">
       <Grid container spacing={1}>
-        {/* <Grid item xs={12} sm={4} md={4} lg={4}>
+        <Grid item xs={12} sm={4} md={4} lg={4}>
           <div className="landing-footer-item-wrap">
             <p className="landing-footer-item-title">Total Staked</p>
+            <p className="landing-footer-item-value">
+              {!tvl ? (
+                <Skeleton width="180px" />
+              ) : (
+                new Intl.NumberFormat('en-US', {
+                  style: 'currency',
+                  currency: 'USD',
+                  maximumFractionDigits: 0,
+                  minimumFractionDigits: 0,
+                }).format(tvl)
+              )}
+            </p>
           </div>
-        </Grid> */}
-        <Grid item xs={12} sm={6} md={6} lg={6}>
+        </Grid>
+        <Grid item xs={12} sm={4} md={4} lg={4}>
           <div className="landing-footer-item-wrap">
             <p className="landing-footer-item-title">Treasury Balance</p>
             <p className="landing-footer-item-value">
@@ -47,7 +62,7 @@ query {
             </p>
           </div>
         </Grid>
-        <Grid item xs={12} sm={6} md={6} lg={6}>
+        <Grid item xs={12} sm={4} md={4} lg={4}>
           <div className="landing-footer-item-wrap">
             <p className="landing-footer-item-title">Current APY</p>
             <p className="landing-footer-item-value">
