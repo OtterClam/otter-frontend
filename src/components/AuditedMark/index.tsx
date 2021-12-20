c0a29bc768c493a61084019747303e25ce1d4111
Otter <king@otterclam.finance>
2021-12-20 15:10:15 +0800

    feat: x2 slowmist logo

--- src/components/AuditedMark/index.tsx
+++ src/components/AuditedMark/index.tsx
@@ -10,7 +10,7 @@ export default function AuditedMark() {
         href="https://www.slowmist.com/en/security-audit-certificate.html?id=4d43b0eb547aa83dc2ff5bef71f99916e33b669a5f30572f1826d7e8220265c2"
         target="_blank"
       >
-        <img src={SlowMist} />
+        <img src={SlowMist} width={80} height={18} />
       </Link>
     </div>
   );
