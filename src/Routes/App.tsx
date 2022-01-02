63b2043e209a75b9800dd96613d646b3f5deaf3d
yangej <yangej0218@gmail.com>
2022-01-02 19:16:17 +0800

    refactor(redux): import action from bond action

--- src/Routes/App.tsx
+++ src/Routes/App.tsx
@@ -16,7 +16,7 @@ import { useAddress, useWeb3Context } from '../hooks';
 import { calculateUserBondDetails, loadAccountDetails } from '../store/slices/account-slice';
 import { loadAppDetails } from '../store/slices/app-slice';
 import { loadTermsDetails } from '../store/slices/otter-lake-slice';
-import { calcBondDetails } from '../store/slices/bond-slice';
+import { calcBondDetails } from '../store/actions/bond-action';
 import { IReduxState } from '../store/slices/state.interface';
 import { Bond, ChooseBond, Stake, Wrap } from '../views';
 import NotFound from '../views/404/NotFound';
