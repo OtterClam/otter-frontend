7735747e9bc03fd53551155f06002815aebd4697
yangej <yangej0218@gmail.com>
2022-01-03 01:49:30 +0800

    refactor(button): make custom btn more flexible

--- src/components/Button/CustomButton.tsx
+++ src/components/Button/CustomButton.tsx
@@ -1,6 +1,6 @@
 import { ElementType, ComponentProps, forwardRef, CSSProperties } from 'react';
 
-import { Box, SvgIcon } from '@material-ui/core';
+import { Box, colors, SvgIcon } from '@material-ui/core';
 
 import './button.scss';
 
@@ -9,6 +9,8 @@ type ButtonType = 'outline' | 'solid' | 'icon';
 type Props = BoxProps & {
   text?: string;
   type?: ButtonType;
+  mainColor?: string;
+  secondaryColor?: string;
   icon?: ElementType<any>;
   href?: string;
   target?: string;
@@ -34,35 +36,20 @@ const STYLE: Record<ButtonType, ButtonConfig> = {
 const ICON_BASE_STYLE: CSSProperties = { width: '20px', height: '20px', verticalAlign: 'middle' };
 const ICON_SPACING: CSSProperties = { marginRight: '10px', marginLeft: '-10px' };
 
-const CustomButton = forwardRef<any, Props>(
-  ({ type = 'solid', className = '', component, text, icon, ...props }, ref) => {
-    component = props.href ? 'a' : component;
-    if (type === 'icon') {
-      return (
-        <Box
-          {...({ ref } as any)}
-          className={'icon-button ' + className}
-          {...STYLE['icon']}
-          component={component}
-          {...props}
-        >
-          {icon && <SvgIcon component={icon} style={{ ...ICON_BASE_STYLE }} />}
-        </Box>
-      );
-    }
+const CustomButton = forwardRef<any, Props>(({ type = 'solid', text, icon, ...props }, ref) => {
+  if (type === 'icon') {
     return (
-      <Box
-        {...({ ref } as any)}
-        className={'custom-button ' + className}
-        {...STYLE[type]}
-        component={component}
-        {...props}
-      >
-        {icon && <SvgIcon component={icon} style={{ ...ICON_BASE_STYLE, ...ICON_SPACING }} />}
-        <p>{text}</p>
+      <Box {...({ ref } as any)} className="icon-button" border="2px solid" {...props}>
+        {icon && <SvgIcon component={icon} style={{ ...ICON_BASE_STYLE }} />}
       </Box>
     );
-  },
-);
+  }
+  return (
+    <Box {...({ ref } as any)} className="custom-button" border={type === 'outline' && '2px solid'} {...props}>
+      {icon && <SvgIcon component={icon} style={{ ...ICON_BASE_STYLE, ...ICON_SPACING }} />}
+      <p>{text}</p>
+    </Box>
+  );
+});
 
 export default CustomButton;
