69192f32f0ada29c54b6b9a25164f8f9ffc1f86c
d0c0q <d0c0q@otterclam.finance>
2022-01-03 09:56:06 +0800

    Revert "Merge pull request #40 from OtterClam/feat/snowing"
    This reverts commit 476aceee4816ab3ac198db1f922be397dc6b5a08, reversing
    changes made to 74f65f5ef8ce9f97bab6f6517e42e9ba9108ac27.

--- src/Routes/App.tsx
+++ src/Routes/App.tsx
@@ -3,11 +3,10 @@ import { makeStyles } from '@material-ui/core/styles';
 import { useCallback, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Redirect, Route, Switch, useLocation } from 'react-router-dom';
-import SnowFall from 'react-snowfall';
-import Calculator from 'src/views/Calculator';
 import Dashboard from 'src/views/Dashboard/TreasuryDashboard';
 import Migrate from 'src/views/Migrate';
 import PearlChests from 'src/views/PearlChests';
+import Calculator from 'src/views/Calculator';
 import TopBar from '../components/Header';
 import Loading from '../components/Loader';
 import Sidebar from '../components/Sidebar';
@@ -167,7 +166,6 @@ function App() {
 
   return (
     <>
-      <SnowFall />
       <div className={`app ${isSmallerScreen && 'tablet'} ${isSmallScreen && 'mobile'}`}>
         <TopBar drawe={!isSmallerScreen} handleDrawerToggle={handleDrawerToggle} />
         <nav className={classes.drawer}>
