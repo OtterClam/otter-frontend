102586dd416f768618e41c22617264654901fc75
yangej <yangej0218@gmail.com>
2022-01-04 00:54:24 +0800

    feat(bond): setup new contract for mai_clam44

--- src/helpers/contract-for-bond.ts
+++ src/helpers/contract-for-bond.ts
@@ -1,16 +1,18 @@
 import { ethers } from 'ethers';
 import { BondKey, getBond } from '../constants';
-import { OtterBond, OtterBondStake, OtterNonStableBondDepository } from '../abi';
+import { OtterBond, OtterBondStake, OtterNonStableBondDepository, OtterPAWBondStakeDepository } from '../abi';
 
 export const contractForBond = (
   bond: BondKey,
   networkID: number,
   provider: ethers.Signer | ethers.providers.Provider,
 ): ethers.Contract => {
-  const { address, autostake, stable } = getBond(bond, networkID);
+  const { address, autostake, key, stable } = getBond(bond, networkID);
+  if (key === 'mai_clam44') {
+    return new ethers.Contract(address, OtterPAWBondStakeDepository, provider);
+  }
   if (stable) {
     return new ethers.Contract(address, autostake ? OtterBondStake : OtterBond, provider);
-  } else {
-    return new ethers.Contract(address, OtterNonStableBondDepository, provider);
   }
+  return new ethers.Contract(address, OtterNonStableBondDepository, provider);
 };
