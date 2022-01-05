7869b5414264cc9f42cba17bcc386c829ad78aec
yangej <yangej0218@gmail.com>
2022-01-05 22:25:19 +0800

    refactor(button): remove unused variables

--- src/components/Button/CustomButton.tsx
+++ src/components/Button/CustomButton.tsx
@@ -16,23 +16,6 @@ type Props = BoxProps & {
   target?: string;
 };
 
-type ButtonConfig = Partial<Pick<BoxProps, 'bgcolor' | 'color' | 'border'>>;
-const STYLE: Record<ButtonType, ButtonConfig> = {
-  outline: {
-    border: '2px solid',
-    bgcolor: '',
-    color: 'text.primary',
-  },
-  solid: {
-    bgcolor: 'otter.otterBlue',
-    color: 'otter.white',
-  },
-  icon: {
-    border: '2px solid',
-    bgcolor: '',
-    color: 'text.primary',
-  },
-};
 const ICON_BASE_STYLE: CSSProperties = { width: '20px', height: '20px', verticalAlign: 'middle' };
 const ICON_SPACING: CSSProperties = { marginRight: '10px', marginLeft: '-10px' };
 
