74af206bafcffb6400973b5ceb5f38333b88d2ac
yangej <yangej0218@gmail.com>
2022-01-22 11:25:35 +0800

    feat(otto): apply page to route

--- src/Routes/Landing.tsx
+++ src/Routes/Landing.tsx
@@ -1,16 +1,23 @@
 import { ThemeProvider } from '@material-ui/core';
+import { useThemeChangedByTime } from 'src/hooks/theme';
 import { Route, Switch } from 'react-router-dom';
 import { dark as darkTheme } from 'src/themes/app';
-import { NFT } from 'src/views';
+import { NFT, Otto } from 'src/views';
 import Landing from '../views/Landing';
 import './style.scss';
 
 function App() {
+  const theme = useThemeChangedByTime();
   return (
     <Switch>
       <Route exact path="/nft">
         <ThemeProvider theme={darkTheme}>
           <NFT />
+        </ThemeProvider>
+      </Route>
+      <Route exact path="/otto">
+        <ThemeProvider theme={theme}>
+          <Otto />
         </ThemeProvider>
       </Route>
       <Route component={Landing} />
