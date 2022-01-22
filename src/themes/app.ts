eccc4c18b29a4d0f8104cb6e09536d6c662d3fd4
yangej <yangej0218@gmail.com>
2022-01-23 01:11:35 +0800

    feat(app): add needed colors

--- src/themes/app.ts
+++ src/themes/app.ts
@@ -13,6 +13,7 @@ const colorPalette = {
   light: {
     otterDark: '#1D2654',
     white: '#FFFFFF',
+    darkBlue: '#0A0E23',
     lightGray100: '#F7F9FB',
     lightGray200: '#E9F0F3',
     lightGray300: '#DBE6EC',
@@ -20,6 +21,7 @@ const colorPalette = {
     darkGray100: '#8193B5',
     darkGray200: '#5E6E99',
     darkGray300: '#434E77',
+    darkGray400: '#303E65',
     highlight: '#FF6854',
     chip: {
       normal: {
@@ -35,6 +37,7 @@ const colorPalette = {
   dark: {
     otterDark: '#F7F9FB',
     white: '#0A0E23',
+    darkBlue: '#FFFFFF',
     lightGray100: '#101631',
     lightGray200: '#212B4F',
     lightGray300: '#303E65',
@@ -42,6 +45,7 @@ const colorPalette = {
     darkGray100: '#8192B4',
     darkGray200: '#A1AEC9',
     darkGray300: '#CCD6E7',
+    darkGray400: '#DBE6EC',
     highlight: '#FF6854',
     chip: {
       normal: {
