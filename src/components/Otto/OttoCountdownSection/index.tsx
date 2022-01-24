cb284eb249fef79a867ea63aba24c4cac85d8d79
yangej <yangej0218@gmail.com>
2022-01-25 00:01:10 +0800

    fix(otto/countdown): replace with coming soon

--- src/components/Otto/OttoCountdownSection/index.tsx
+++ src/components/Otto/OttoCountdownSection/index.tsx
@@ -24,13 +24,14 @@ const OttoCountdownSection = () => {
     <div className={`otto-countdown__container ${classes.container}`}>
       <img className="otto-countdown__deco left" src={OttoDecoLeftImage} />
       <h4 className={`otto-countdown__title ${classes.h4}`}>{t('otto.countdown.mintTitle')}</h4>
-      <Countdown
+      <p className="otto-countdown__content">COMING SOON</p>
+      {/* <Countdown
         title={t('otto.countdown.mintSubtitle')}
         color="mode.white"
         bgcolor="mode.darkBlue"
         dueDate={new Date()}
         onTimeUp={() => {}}
-      />
+      /> */}
       {/* TODO|OTTO: add time up callback */}
       <OttoBuyClamHint />
       <img className="otto-countdown__deco right" src={OttoDecoRightImage} />
