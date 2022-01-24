9a45c103ac7a6eb8db6e56313da1df29baa1ffe0
Otter <king@otterclam.finance>
2022-01-25 00:49:49 +0800

    feat: add new ads for otto

--- src/components/DashboardHero/index.tsx
+++ src/components/DashboardHero/index.tsx
@@ -3,11 +3,21 @@ import Ad1 from './ad-01.jpg';
 import Ad2 from './ad-02.jpg';
 import Ad3 from './ad-03.jpg';
 import Ad4 from './ad-04.jpg';
+import Ad5 from './ad-05.gif';
+import Ad6 from './ad-06.gif';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import './styles.scss';
 
 export default function DashboardHero() {
   const ads = [
+    {
+      image: Ad5,
+      link: 'https://www.otterclam.finance/#/otto',
+    },
+    {
+      image: Ad6,
+      link: 'https://www.otterclam.finance/#/otto',
+    },
     {
       image: Ad1,
       link: '',
