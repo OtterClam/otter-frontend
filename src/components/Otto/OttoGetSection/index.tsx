d71721750e5153f48751de5167e352a55d6fe58f
yangej <yangej0218@gmail.com>
2022-01-24 21:35:40 +0800

    feat(otto/get): add i18n desc

--- src/components/Otto/OttoGetSection/index.tsx
+++ src/components/Otto/OttoGetSection/index.tsx
@@ -1,7 +1,9 @@
+import { useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
 import { Typography, makeStyles } from '@material-ui/core';
 import OttoStepBox from './OttoStepBox';
 
-import { OTTO_STEP_METADATA } from './constant';
+import { DescriptionMetadata } from './type';
 
 import './style.scss';
 
@@ -17,6 +19,40 @@ const useStyles = makeStyles(theme => ({
 
 const OttoGetSection = () => {
   const classes = useStyles();
+  const { t } = useTranslation();
+  const OTTO_STEP_METADATA: DescriptionMetadata[] = useMemo(
+    () => [
+      {
+        imgSrc: '',
+        description: [
+          { type: 'normal', text: t('otto.get.discord1') },
+          { type: 'highlight', text: t('otto.get.discordHighlight') },
+          { type: 'normal', text: t('otto.get.discord2') },
+        ],
+        buttonText: t('otto.get.discordButton'),
+      },
+      {
+        imgSrc: '',
+        description: [
+          { type: 'normal', text: t('otto.get.buyClam1') },
+          { type: 'highlight', text: t('otto.get.buyClamHighlight') },
+          { type: 'normal', text: t('otto.get.buyClam2') },
+        ],
+        buttonText: t('otto.get.butClamButton'),
+      },
+      {
+        imgSrc: '',
+        description: [
+          { type: 'normal', text: t('otto.get.calendar1') },
+          { type: 'highlight', text: t('otto.get.calendarHighlight') },
+          { type: 'normal', text: t('otto.get.calendar2') },
+        ],
+        buttonText: t('otto.get.calendarButton'),
+      },
+    ],
+    [],
+  );
+
   return (
     <div className={`otto-get__container container ${classes.container}`}>
       <Typography className={`${classes.h4} otto-get__title`} variant="h4">
