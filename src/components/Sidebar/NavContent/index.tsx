0e496598a3ea3d1812c554fb755ffdbb90229b76
0xthk <kf013099@gmail.com>
2022-05-15 10:54:37 +0800

    Revert "fix: remove quickswap link tmp"
    This reverts commit 315f587d6b48653572d750b1c6475837c96a03c4.

--- src/components/Sidebar/NavContent/index.tsx
+++ src/components/Sidebar/NavContent/index.tsx
@@ -260,7 +260,12 @@ function NavContent() {
                 </div>
               </Link>
 
-              <Link href="" target="_blank" rel="noreferrer" className="button-dapp-menu">
+              <Link
+                href={'https://quickswap.exchange/#/swap?outputCurrency=' + CLAM_ADDRESS}
+                target="_blank"
+                rel="noreferrer"
+                className="button-dapp-menu"
+              >
                 <div className="dapp-menu-item">
                   <IconDefaultBuyClam />
                   <p>{t('common.buyThing')}CLAM</p>
