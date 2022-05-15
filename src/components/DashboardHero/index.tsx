18196860b442c08d79a52a681db096021a6f1569
0xthk <kf013099@gmail.com>
2022-05-15 15:03:27 +0800

    feat: update ads

--- src/components/DashboardHero/index.tsx
+++ src/components/DashboardHero/index.tsx
@@ -1,33 +1,28 @@
 import { Carousel } from 'react-responsive-carousel';
-import Ad3 from './ad-03.jpg';
-import Ad4 from './ad-04.jpg';
-import Ad0211 from './ad-0211.jpg';
-import Ad0320 from './ad-0320.jpg';
-import Ad0319 from './ad-0319.jpg';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import Ad0319 from './ad-0319.jpg';
+import Ad0509 from './ad-0509.jpg';
+import Ad0510 from './ad-0510.jpg';
+import Ad0511 from './ad-0511.jpg';
 import './styles.scss';
 
 export default function DashboardHero() {
   const ads = [
     {
-      image: Ad0320,
-      link: 'https://ottopia.app',
+      image: Ad0509,
+      link: 'https://ottopia.app/leaderboard',
     },
     {
-      image: Ad0211,
-      link: 'https://www.otterclam.finance/',
+      image: Ad0510,
+      link: 'https://ottopia.app/store',
     },
     {
       image: Ad0319,
-      link: 'https://www.youtube.com/watch?v=b-sXdS24XS4',
+      link: 'https://www.youtube.com/watch?v=9wA-6A2ONnA',
     },
     {
-      image: Ad3,
-      link: 'https://www.sandbox.game/en/estates/624/',
-    },
-    {
-      image: Ad4,
-      link: 'https://www.youtube.com/watch?v=laPZp3WUt6I',
+      image: Ad0511,
+      link: 'https://www.youtube.com/watch?v=xIgPCJZuPfU',
     },
   ];
   return (
