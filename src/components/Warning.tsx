561d87eb01afa9910160e9ddc8e5889170993ee1
0xthk <kf013099@gmail.com>
2022-07-16 09:27:23 +0800

    fix: invalid date

--- src/components/Warning.tsx
+++ src/components/Warning.tsx
@@ -3,7 +3,7 @@ import { useTranslation } from 'react-i18next';
 
 function Warning() {
   const { t } = useTranslation();
-  const time = new Date('2022-7-16 00:00:00Z').toLocaleString();
+  const time = new Date(1657929600000).toLocaleString();
   return (
     <Box bgcolor="#F5523C" color="white" padding="20px 40px">
       <p style={{ fontSize: '20px', lineHeight: '30px' }}>
